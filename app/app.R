e3fe727147cadbdc1b2a1cbd69ff367cc1d0627b
MantasKas <119167556+MantasKas@users.noreply.github.com>
2022-12-19 21:48:24 +0200

    Add files via upload

---
+++ app/app.R
@@ -0,0 +1,42 @@
+#===============================================================================
+#SHINY APLIKACIJOS KŪRIMAS
+#===============================================================================
+
+library(shiny)
+library(tidyverse)
+library(h2o)
+
+ui <- fluidPage(
+  
+  titlePanel("Banking app"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Ikelkite faila:")
+    ),
+    mainPanel(
+      tableOutput("table")
+    )
+  )
+)
+server <- function(input, output) {
+  h2o.init()
+  model <- h2o.loadModel("C:/Users/Mantas/Downloads/my_model_gbm")
+  
+  output$table <- renderTable({
+    req(input$file$datapath)
+    df <- h2o.importFile(input$file$datapath)
+    results <- h2o.predict(model, df)
+    
+    results %>%
+      as_tibble() %>%
+      mutate(id = row_number(), y = p0, result = ifelse(predict == 0, "Gera paskola", "Bloga paskola")) %>%
+      select(id, result, y) %>%
+      head()
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+h2o.shutdown()
